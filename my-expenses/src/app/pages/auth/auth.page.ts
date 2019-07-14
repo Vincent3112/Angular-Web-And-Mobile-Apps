31b9d3395ca249d2ebe5d5de2800795456387f6b
unknown <vincent.granie@2018.icam.fr>
2019-07-14 21:55:53 +0200

    Improved login page

--- my-expenses/src/app/pages/auth/auth.page.ts
+++ my-expenses/src/app/pages/auth/auth.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-auth',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthPage implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private navCtrl: NavController, private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6)
+      ]))
+    });
+  }
+
 
   ngOnInit() {
   }
