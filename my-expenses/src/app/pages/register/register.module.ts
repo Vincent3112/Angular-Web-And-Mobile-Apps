4d21df98ac49640bb77dd5f0ea11f4a2370df06f
unknown <vincent.granie@2018.icam.fr>
2019-07-17 22:27:40 +0200

    refactored auth page and register page

--- my-expenses/src/app/pages/register/register.module.ts
+++ my-expenses/src/app/pages/register/register.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
+import { MaterialModule } from '../../material.module';
 import { IonicModule } from '@ionic/angular';
-
 import { RegisterPage } from './register.page';
 
 const routes: Routes = [
@@ -19,8 +18,9 @@ const routes: Routes = [
     CommonModule,
     ReactiveFormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MaterialModule
   ],
   declarations: [RegisterPage]
 })
-export class RegisterPageModule {}
+export class RegisterPageModule { }
